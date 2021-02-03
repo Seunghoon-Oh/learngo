013b81b2867d4fcfe2fc9d91815e77cc4a80d6c4
Seunghoon-Oh <steveoh5419@gmail.com>
2021-02-03 21:27:01 +0900

    #2.3 Finishing Up

--- main.go
+++ main.go
@@ -3,16 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/Seunghoon-Oh/learngo/accounts"
+	"github.com/serranoarevalo/learngo/accounts"
 )
 
 func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
 }
