00b8d1e34bbfd1234714cc266efe2580946d4371
Seunghoon-Oh <steveoh5419@gmail.com>
2021-02-05 13:20:45 +0900

    #5.1 Setup Part Two

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "github.com/Seunghoon-Oh/learngo/scrapper"
+import (
+	"fmt"
+	"learngo/scrapper"
+	"strings"
+
+	"github.com/labstack/echo"
+)
+
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	fmt.Println(term)
+	return nil
+}
 
 func main() {
-	scrapper.Scrape("term")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
